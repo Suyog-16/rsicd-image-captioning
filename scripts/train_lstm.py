ef8d07b537a2d02631b5e18db256161cfdf412f1
Suyog-16 <ghimiresuyog89@gmail.com>
2025-12-22 22:55:33 +0545

    Refactor train_lstm.py by removing custom RSICD dataset class and reorganizing imports for clarity

--- scripts/train_lstm.py
+++ scripts/train_lstm.py
@@ -2,65 +2,33 @@
 import os
 import yaml
 import json
+import random
 import torch
 import torch.nn as nn
 import torchvision.transforms as T
 from torchvision import models
+from torch.utils.data import DataLoader,Dataset
+
 from PIL import Image
 from typing import List, Dict
+
 from models.decoder_lstm import Decoder
 from models.encoder import Encoder
 from models.cnn_lstm_model import ImageCaptioningModel
+
 from utils.seeds import set_seed
 from utils.logging import save_config,create_writer
 from utils.load_configs import load_config
+from utils.collate_function import collate_fn
+from utils.preprocess import build_vocab
+from models.dataset import RSICD
 
+
+set_seed(42)
 
 config = load_config("config.yaml")
-
-
-#--------------- Dataset Class Adapted from authors : https://github.com/isaaccorley/torchrs --------
-
-class RSICD(torch.utils.data.Dataset):
-    """ Image Captioning Dataset from 'Exploring Models and Data for
-    Remote Sensing Image Caption Generation', Lu et al. (2017)
-    https://arxiv.org/abs/1712.07835
-
-    'RSICD is used for remote sensing image captioning task. more than ten thousands
-    remote sensing images are collected from Google Earth, Baidu Map, MapABC, Tianditu.
-    The images are fixed to 224X224 pixels with various resolutions. The total number of
-    remote sensing images are 10921, with five sentences descriptions per image.'
-    """
-    splits = ["train", "val", "test"]
-
-    def __init__(
-        self,
-        root: str = "data",
-        split: str = "train",
-        transform: T.Compose = T.Compose([T.ToTensor()])
-    ):
-        assert split in self.splits
-        self.root = root
-        self.transform = transform
-        self.captions = self.load_captions(os.path.join(root, "dataset_rsicd.json"), split)
-        self.image_root = "RSICD_images"
-
-    @staticmethod
-    def load_captions(path: str, split: str) -> List[Dict]:
-        with open(path) as f:
-            captions = json.load(f)["images"]
-        return [c for c in captions if c["split"] == split]
-
-    def __len__(self) -> int:
-        return len(self.captions)
-
-    def __getitem__(self, idx: int) -> Dict:
-        captions = self.captions[idx]
-        path = os.path.join(self.root, self.image_root, captions["filename"])
-        x = Image.open(path).convert("RGB")
-        x = self.transform(x)
-        sentences = [sentence["raw"] for sentence in captions["sentences"]]
-        return dict(x=x, captions=sentences)
+#---------- Making it Device agnostic---------------------------------
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
 #----------------------- Model Defination -------------------------
@@ -75,5 +43,16 @@ base_model = ImageCaptioningModel(encoder,decoder)
 #----------------------- Loss functions,optimizers and logging------------------
 EPOCHS = config['training']['epochs']
 learning_rate = config['training']['learning_rate']
+BATCH_SIZE = config['training']['batch_size']
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(base_model.parameters(),lr = learning_rate)
+
+#--------------- Dataset Class Adapted from authors : https://github.com/isaaccorley/torchrs --------
+train_dataset = RSICD(root="data",split='train')
+all_train_captions = [cap for item in train_dataset for cap in item['captions']]
+word2idx, idx2word = build_vocab(all_train_captions, min_freq=2)
+train_loader = DataLoader(train_dataset,batch_size=BATCH_SIZE,collate_fn= lambda batch: collate_fn(batch,word2idx))
+
+
+
+
