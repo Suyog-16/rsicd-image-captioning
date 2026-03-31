dd080585dc6c0fa7adcdf4b979ea36ade790f984
Suyog-16 <ghimiresuyog89@gmail.com>
2026-03-31 18:32:24 +0545

    Remove unused vocabulary building code from collate function

--- utils/collate_function.py
+++ utils/collate_function.py
@@ -1,13 +1,7 @@
 import torch
 from torch.nn.utils.rnn import pad_sequence
-from .preprocess import preprocess_fn, caption_to_indices, build_vocab
-from models.dataset import RSICD
+from .preprocess import preprocess_fn, caption_to_indices
 import random
-
-# Load train split and build vocabulary
-train_dataset = RSICD(root="data", split='train')
-all_train_captions = [cap for item in train_dataset for cap in item['captions']]
-word2idx, idx2word = build_vocab(all_train_captions, min_freq=2)
 
 # Collate function that takes word2idx as argument
 def collate_fn(batch, word2idx):
